3e1d9c072ff4e4253f23b3e6fee99aa47d714643
Revaliza123 <116142603+Revaliza123@users.noreply.github.com>
2023-10-06 15:52:01 +0700

    Update data-masuk.blade.php

--- resources/views/admin/masuk/data-masuk.blade.php
+++ resources/views/admin/masuk/data-masuk.blade.php
@@ -49,6 +49,15 @@
             </div>
 
             <div class="container p-5 col-lg-10">
+                      @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
                 @if (Session::has('status'))
                   <div class="alert alert-success" role="alert">
                     {{ Session::get('message') }}
